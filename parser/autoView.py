c331d5f86309fbf7e6f368e622dc2c54a76a9f9d
Lactobacillus <cyw314@gmail.com>
2018-10-29 01:37:40 +0900

    asdf

--- parser/autoView.py
+++ parser/autoView.py
@@ -1,2 +1,70 @@
 # parsing http://www.autoview.co.kr/
 # 뉴스 (전체), 구매가이드 (시승기, 리콜 정보)
+
+import os
+import sys
+import pickle
+import urllib.request
+from bs4 import BeautifulSoup
+
+if __name__ == '__main__':
+
+	# 뉴스 (전체)
+	docURLs = list()
+	docs = list()
+	baseURL = 'http://www.autoview.co.kr/content/news/news_total_list.asp?page={}'
+	
+	for idx in range(1, 2):
+	#for idx in range(1, 2000 + 1):
+
+		page = urllib.request.urlopen(baseURL.format(idx))
+		soup = BeautifulSoup(page, 'lxml')
+		soup = soup.find('div', class_ = 'article').find_all('li')
+
+		for li in soup:
+
+			docURLs.append(li.find('a')['href'])
+
+	
+	for docURL in docURLs:
+
+		page = urllib.request.urlopen('http://www.autoview.co.kr' + docURL)
+		soup = BeautifulSoup(page, 'html')
+		
+		title = soup.find('h4').text.strip()
+		time = soup.find('div', class_ = 'article_info').getText().strip()[8:18]
+
+		
+		#soup = soup.find('div', class_ = 'article_text').find('div', class_ = 'article_text')
+		
+
+
+
+		#print(soup.getText().strip().replace('\n', ' '))
+		#print('==========================================')
+
+
+
+
+		#mainText = ''
+		
+		a = Article(docURL, language = 'ko')
+		a.download()
+		a.parse()
+
+		print(a.text)
+		print('========================')
+
+
+
+		"""
+		for p in soup:
+
+			mainText = mainText + p.getText().replace('\n', '')
+
+		docs.append({'title' : title, 'time' : time, 'content' : mainText})
+
+	with open('autiview_news.pickle', 'wb') as fs:
+
+		pickle.dump(docs, fs)
+		"""
--- parser/motorGraph.py
+++ parser/motorGraph.py
@@ -6,17 +6,16 @@ import sys
 import pickle
 import urllib.request
 from bs4 import BeautifulSoup
-#from newspaper import Article
 
 if __name__ == '__main__':
 
-	# 최신뉴스 (전체) 에 {업계/정책 (완성차), 시승기/리뷰 (국산차, 수입차), 결함/문제점 (리콜)} 이 모두 포함
+	# 최신뉴스 (전체) ==> [업계/정책 (완성차), 시승기/리뷰 (국산차, 수입차), 결함/문제점 (리콜)] 이 모두 포함
 	docURLs = list()
 	docs = list()
 	baseURL = 'http://www.motorgraph.com/news/articleList.html?page={}'
 	
 	for idx in range(1, 2):
-	#for idx in range(1, 700):
+	#for idx in range(1, 700 + 1):
 
 		page = urllib.request.urlopen(baseURL.format(idx))
 		soup = BeautifulSoup(page, 'lxml')
@@ -33,10 +32,9 @@ if __name__ == '__main__':
 		
 		title = soup.find('div', class_ = 'article-head-title').text.strip()
 		time = soup.find('div', class_ = 'info-text').find_all('li')[2].text.strip()[3:13]
+		soup = soup.find('div', id = 'articleBody').find_all('p')
 		
 		mainText = ''
-
-		soup = soup.find('div', id = 'articleBody').find_all('p')
 		
 		for p in soup:
 
@@ -47,4 +45,47 @@ if __name__ == '__main__':
 	with open('motorgraph_news.pickle', 'wb') as fs:
 
 		pickle.dump(docs, fs)
+
+	# 커뮤니티 (자동차 정보/토론)
+	docURLs = list()
+	docs = list()
+	baseURL = 'https://c.motorgraph.com/index.php?mid=info&page={}'
+	
+	for idx in range(1, 2):
+	#for idx in range(1, 15 + 1):
+
+		page = urllib.request.urlopen(baseURL.format(idx))
+		soup = BeautifulSoup(page, 'lxml')
+		soup = soup.find('table').find_all('a', class_ = 'hx')
+
+		for t in soup:
+
+			docURLs.append(t['href'])
+
+	for docURL in docURLs:
+
+		comments = list()
+
+		page = urllib.request.urlopen(docURL)
+		soup = BeautifulSoup(page, 'html')
 		
+		title = soup.find('h1', class_ = 'np_18px').text.split('            ')[1]
+		time = soup.find('span', class_ = 'reg-date').text.strip()[0:10]
+		mainText = soup.find('article', itemprop = 'articleBody').getText().replace('\n', ' ')
+
+		soup = soup.find_all('div', id = 'cmtPosition')
+
+		# 댓글이 있는 경우
+		if len(soup) == 1:
+
+			cmts = soup[0].find_all('li')
+
+			for cmt in cmts:
+
+				comments.append(cmt.find_all('div')[1].getText().strip().replace('\n', ' '))
+
+		docs.append({'title' : title, 'time' : time, 'content' : mainText, 'reply' : comments})
+
+	with open('motorgraph_board.pickle', 'wb') as fs:
+
+		pickle.dump(docs, fs)

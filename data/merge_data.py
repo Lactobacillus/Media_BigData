33e2971973af6b40368ef3889b49ce48a4630173
Lactobacillus <cyw314@gmail.com>
2018-11-14 01:31:56 +0900

    sdaf

---
+++ data/merge_data.py
@@ -0,0 +1,34 @@
+import sys
+import pickle
+from datetime import datetime
+
+if __name__ == '__main__':
+
+	dataList = list()
+
+	for arg in sys.argv[1:]:
+
+		with open(arg, 'wb') as fs:
+
+			data = pickle.load(fs)
+
+		for d in data:
+
+			try:
+
+				d['time'] = d['time'].replace('-', '.')
+				d['time'] = datetime.strptime(d['time'], '%Y.%m.%d')
+
+			except:
+
+				pass
+
+			else:
+
+				dataList.append(d)
+
+	print('# of total data : ', len(dataList))
+
+	with open('integrated_data.pickle', 'wb') as fs:
+
+		pickle.dump(dataList, fs)

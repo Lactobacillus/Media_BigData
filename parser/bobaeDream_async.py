eb4f72842c86a531b6dd50709c390172c16b6df4
Lactobacillus <cyw314@gmail.com>
2018-11-17 00:29:26 +0900

    gg

--- parser/bobaeDream_async.py
+++ parser/bobaeDream_async.py
@@ -5,7 +5,9 @@ import os
 import sys
 import pickle
 import asyncio
+import itertools
 import multiprocessing
+from time import sleep
 from bs4 import BeautifulSoup
 from joblib import Parallel, delayed
 from aiohttp import ClientSession as cs
@@ -31,31 +33,74 @@ def parseURL(html):
 
 	return docURLs
 
+def parseArticle(html):
+
+	docs = list()
+
+
+
 if __name__ == '__main__':
 
-	loop = asyncio.get_event_loop()
-
-	baseURL = 'http://www.bobaedream.co.kr/list?code=national&page={}'
-
+	# 국산차게시판
 	docURLs = list()
+	docHTMLs = list()
 	docs = list()
 	task = list()
+	baseURL = 'http://www.bobaedream.co.kr/list?code=national&page={}'
 
+	loop = asyncio.get_event_loop()
+	
 	for idx in range(1, 10):
 	#for idx in range(1, 25000 + 1):
 
 		task.append(asyncio.ensure_future(getOnePage(baseURL.format(idx))))
 
+		if idx % 100 == 0:
 
-	loop.run_until_complete(asyncio.wait(task))
+			loop.run_until_complete(asyncio.wait(task))
+			sleep(2)
+
+	loop.close()
+
+	docURLs = list(itertools.chain.from_iterable(Parallel(
+		n_jobs = multiprocessing.cpu_count())(delayed(parseURL)(page) for page in [t.result() for t in task])))
 
 
-	result = [t.result() for t in task]
 
-	ll = Parallel(n_jobs = multiprocessing.cpu_count())(delayed(parseURL)(page) for page in result)
+"""
 
-	for l in ll:
+	for docURL in docURLs:
 
-		docURLs = docURLs + l
+		try:
 
-	print(docURLs)
+			comments = list()
+
+			page = urllib.request.urlopen('http://www.bobaedream.co.kr' + docURL)
+			soup = BeautifulSoup(page, 'lxml')
+			
+			title = soup.find('div', class_ = 'writerProfile').find('strong').text.strip().split('[')[0]
+			time = soup.find('span', class_ = 'countGroup').text[-20:-10]
+			mainText = soup.find('div', class_ = 'bodyCont').text.strip().replace('\n', ' ').replace('\r', '')
+
+			soup = soup.find('div', id = 'cmt_list')
+
+			# 댓글
+			if int(soup.find('span', class_ = 'comm2').text.replace('(', '').replace(')', '')) > 0:
+
+				for li in soup.find('ul', id = 'cmt_reply').find_all('li'):
+
+					comments.append(li.find('dd').text.strip())
+
+			docs.append({'title' : title, 'time' : time, 'content' : mainText, 'reply' : comments})
+
+		except:
+
+			pass
+
+
+
+
+	with open('bobaedream_board_national.txt', 'w') as fs:
+
+		fs.write(str(docs))
+"""
